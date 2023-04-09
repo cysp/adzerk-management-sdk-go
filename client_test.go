bbfc605c33451609ba0cef7ea23b9493b71cf061
Scott Talbot <s@chikachow.org>
2023-04-09 17:48:17 +1000

    feat: generate client with oapi-codegen

--- client_test.go
+++ client_test.go
@@ -1,1 +1,26 @@
 package client
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type httpRequestDoer struct {
+	do func(req *http.Request) (*http.Response, error)
+}
+
+func (h *httpRequestDoer) Do(req *http.Request) (*http.Response, error) {
+	return h.do(req)
+}
+
+func TestNewClient(t *testing.T) {
+	c, err := NewClientWithResponses("https://example.org/adzerk/api/", WithHTTPClient(&httpRequestDoer{do: func(req *http.Request) (*http.Response, error) {
+		return nil, nil
+	}}))
+	require.NoError(t, err)
+
+	assert.NotNil(t, c)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/cysp/adzerk-management-sdk-go
 
 go 1.20
+
+require (
+	github.com/deepmap/oapi-codegen v1.12.4
+	github.com/stretchr/testify v1.8.2
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
